246a79533236aa188e12133babf6446fa413f958
Aleksandr Shevelev <leksevi@icloud.com>
2022-12-08 02:06:36 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число: ");
+double N = double.Parse(Console.ReadLine()!);
+double M = 1;
+
+while(N>=M)
+{
+  double sqM = Math.Pow(M,3);
+    Console.Write("\t" +sqM);
+    M=M+1;
+}
